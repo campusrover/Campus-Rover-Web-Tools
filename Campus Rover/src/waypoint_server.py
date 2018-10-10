67d53deb22d905ff32d63c2c48dd820a9231b16c
Ari Carr <acarr@brandeis.edu>
2018-10-10 00:07:59 -0400

    Added Volen 1 locations

--- Campus Rover/src/waypoint_server.py
+++ Campus Rover/src/waypoint_server.py
@@ -8,30 +8,31 @@ from yocs_msgs.msg import Waypoint, WaypointList
 from geometry_msgs.msg import Pose, Point, Quaternion
 from campus_rover.srv import WaypointsResponse, Waypoints
 import json
-import os 
+import os
 
 rospy.init_node('waypoint_server')
 
 def waypoint_list(arg):
 	dir_path = os.path.dirname(os.path.realpath('waypoints.json'))
-	data = json.load(open('/home/turtlebot/turtlebot_ws/src/campus_rover/src/waypoints.json'))
+	data = json.load(open('/Users/Ari/Dropbox/Robotics/catkin_ws/src/Campus-Rover-Web-Tools/Campus Rover/src/waypoints.json'))
 	waypoints = []
 	for waypoint in data:
-	
+
 		name = waypoint['name']
 		location = waypoint['location']
-		
+		orientation = waypoint['orientation']
+
 		pos = Point(location['x'], location['y'], location['z'])
-		orientation = Quaternion(0, 0, 0, 1)
+		orientation = Quaternion(orientation['x'], orientation['y'], orientation['z'], orientation['w'])
 		pose = Pose(pos, orientation)
-		
+
 		header = std_msgs.msg.Header()
 		header.stamp = rospy.Time.now()
-		
+
 		waypoint = Waypoint(header, name, pose)
 		waypoints.append(waypoint)
-		
-	waypoints = WaypointList(waypoints)	
+
+	waypoints = WaypointList(waypoints)
 	print waypoints
 	response = WaypointsResponse(waypoints)
 
@@ -39,6 +40,3 @@ def waypoint_list(arg):
 
 service = rospy.Service('waypoints', Waypoints, waypoint_list)
 rospy.spin()
-
-
-
