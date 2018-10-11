8ab6a988bf62ff4cb5f8ccefcc93d7a349763745
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2018-10-10 21:35:06 -0400

    Updates to Web Client Bring-Up script

--- web_client_bringup.sh
+++ web_client_bringup.sh
@@ -11,9 +11,15 @@ echo 2) Bringing up TurtleBot2... && roslaunch turtlebot_bringup concert_client.
 # Start the map server:
 echo 3) Starting map_server... && rosrun map_server map_server /home/turtlebot/testmap.yaml & #<--- Update yaml file name
 
+# Start the waypoint server:
+echo 4) Starting waypoint server... && rosrun campus_rover_gen2 waypoint_server.py &
+
+# Start the robot vitals server:
+echo 5) Starting robot vitals server... && rosrun campus_rover robot_vitals.py &
+
 # Start amcl for SLAM navigation:
 export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml & #<--- Update yaml file name
-echo 4) Starting amcl... && roslaunch turtlebot_navigation amcl_demo.launch &
+echo 6) Starting amcl... && roslaunch turtlebot_navigation amcl_demo.launch &
 
 # Enable websocket connections between clients & TurtleBot2
-echo 5) Making websocket connections available... && roslaunch rosbridge_server rosbridge_websocket.launch &
+echo 7) Making websocket connections available... && roslaunch rosbridge_server rosbridge_websocket.launch &
