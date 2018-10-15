ea2ef9a7d6ed6bcb21c6593e421ccec1d8c1ff53
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2018-10-14 20:41:36 -0400

    Commented shell script

--- web_client_bringup.sh
+++ web_client_bringup.sh
@@ -21,5 +21,7 @@
 # Enable websocket connections between clients & TurtleBot2
 # --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"'
 
+# Spin up the locally hosted React web app:
+# --tab -e '/bin/bash -c "npm start --prefix /home/turtlebot/turtlebot_ws/src/Campus-Rover-Web-Tools"'
 
 gnome-terminal --tab -e '/bin/bash -c "roscore"' --tab -e '/bin/bash -c "sleep 10; roslaunch turtlebot_bringup concert_client.launch"' --tab -e '/bin/bash -c "sleep 10; rosrun map_server map_server /home/turtlebot/testmap.yaml"' --tab -e '/bin/bash -c "sleep 10; export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"' --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"' --tab -e '/bin/bash -c "npm start --prefix /home/turtlebot/turtlebot_ws/src/Campus-Rover-Web-Tools"'
