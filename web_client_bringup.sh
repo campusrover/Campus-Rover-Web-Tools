784eb63256d8076f5b790e65700b0424b6567734
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2018-10-14 20:15:06 -0400

    Updates comments on bring-up script

--- web_client_bringup.sh
+++ web_client_bringup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Start the ros_core:
-# echo "1) Starting ros_core..." && gnome-terminal -e roscore
+# --tab -e '/bin/bash -c "roscore"'
+
+# Wait ~10 seconds for roscore to initialize...
 
 # Bring up TurtleBot2 with video feed:
-# echo "2) Bringing up TurtleBot2..." && gnome-terminal -e roslaunch turtlebot_bringup concert_client.launch
+# --tab -e '/bin/bash -c "sleep 10; roslaunch turtlebot_bringup concert_client.launch"'
+
 # OR bring up TurtleBot2 WITHOUT video feed:
 # roslaunch turtlebot_bringup minimal.launch
 
 # Start the map server:
-# echo "3) Starting map_server..." && gnome-terminal -e rosrun map_server map_server /home/turtlebot/testmap.yaml #<--- Update yaml file name
+# --tab -e '/bin/bash -c "sleep 10; rosrun map_server map_server /home/turtlebot/testmap.yaml"'
 
 # Start amcl for SLAM navigation:
  #<--- Update yaml file name
-# echo "4) Starting amcl..." && gnome-terminal -x bash -c "export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"
+# --tab -e '/bin/bash -c "sleep 10; export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"'
 
 # Enable websocket connections between clients & TurtleBot2
-# echo "5) Making websocket connections available..." && gnome-terminal -e roslaunch rosbridge_server rosbridge_websocket.launch
+# --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"'
 
 
 gnome-terminal --tab -e '/bin/bash -c "roscore"' --tab -e '/bin/bash -c "sleep 10; roslaunch turtlebot_bringup concert_client.launch"' --tab -e '/bin/bash -c "sleep 10; rosrun map_server map_server /home/turtlebot/testmap.yaml"' --tab -e '/bin/bash -c "sleep 10; export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"' --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"'
