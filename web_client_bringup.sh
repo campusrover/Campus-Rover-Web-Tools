495531d3579f684fa9a73079ff06be28516f1f54
Ari Carr <acarr@brandeis.edu>
2018-10-16 22:32:44 -0400

    Cleaned up bringup script

--- web_client_bringup.sh
+++ web_client_bringup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-# Start the ros_core:
-# --tab -e '/bin/bash -c "roscore"'
+PREFIX="--tab -e '/bin/bash -c "
 
-# Wait ~10 seconds for roscore to initialize...
+# Start the roscore
+CMD="gnome-terminal ${PREFIX} \"roscore\"'"
 
-# Bring up TurtleBot2 with video feed:
-# --tab -e '/bin/bash -c "sleep 10; roslaunch turtlebot_bringup concert_client.launch"'
+CMD_ARR=(
+# Bring up the robot with video feed
+"roslaunch turtlebot_bringup concert_client.launch"
+# Start the map server
+"rosrun map_server map_server /home/turtlebot/testmap.yaml"
+# Start AMCL
+"export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"
+# Enable websocket connections
+"roslaunch rosbridge_server rosbridge_websocket.launch"
+# Start the waypoint server to list potential TB2 destinations
+"rosrun campus_rover_gen2 waypoint_server.py"
+# Start the web server
+"npm start --prefix ~/turtlebot_ws/src/Campus-Rover-Web-Tools/Web\ Client"
+)
 
-# OR bring up TurtleBot2 WITHOUT video feed:
-# roslaunch turtlebot_bringup minimal.launch
+for curr_cmd in "${CMD_ARR[@]}"
+do
+    :
+    CMD="${CMD} ${PREFIX} \"sleep 10; $curr_cmd\"'"
+done
 
-# Start the map server:
-# --tab -e '/bin/bash -c "sleep 10; rosrun map_server map_server /home/turtlebot/testmap.yaml"'
-
-# Start amcl for SLAM navigation:
- #<--- Update yaml file name
-# --tab -e '/bin/bash -c "sleep 10; export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"'
-
-# Enable websocket connections between clients & TurtleBot2
-# --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"'
-
-# Spin up the locally hosted React web app:
-# --tab -e '/bin/bash -c "npm start --prefix /home/turtlebot/turtlebot_ws/src/Campus-Rover-Web-Tools"'
-
-gnome-terminal --tab -e '/bin/bash -c "roscore"' --tab -e '/bin/bash -c "sleep 10; roslaunch turtlebot_bringup concert_client.launch"' --tab -e '/bin/bash -c "sleep 10; rosrun map_server map_server /home/turtlebot/testmap.yaml"' --tab -e '/bin/bash -c "sleep 10; export TURTLEBOT_MAP_FILE=/home/turtlebot/testmap.yaml; roslaunch turtlebot_navigation amcl_demo.launch"' --tab -e '/bin/bash -c "sleep 10; roslaunch rosbridge_server rosbridge_websocket.launch"' --tab -e '/bin/bash -c "npm start --prefix /home/turtlebot/turtlebot_ws/src/Campus-Rover-Web-Tools"'
+eval $CMD
